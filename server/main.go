34f3e771e09419f77b521e203d97549a482090e3
Andrew Pikul <ajpikul@gmail.com>
2020-06-14 18:23:29 +0000

    Move SSHD code form server main into own file

--- server/main.go
+++ server/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
 	"flag"
 	"log"
-	"net"
-	"fmt"
-	"encoding/binary"
-	"io"
-	"os/exec"
-	"syscall"
-	"sync"
-	"unsafe"
 
-	"github.com/kr/pty"
-	"golang.org/x/crypto/ssh"
+	"github.com/mangodx/AccessTunnel/sshoverws"
+	"github.com/gorilla/mux"
 )
+
 var hostPrivateKey = flag.String("hostkey", "", "Path to the your private key")
 
 func init(){
@@ -25,140 +18,17 @@ func init(){
 	}
 }
 
+	// ESTABLISH SSH CONNETION DOWNPIPE
+
+func handleProxy(w http.ResponseWriter, r *http.Request) {
+	conn, err := sshoverws.Upgrade(w, r)
+	if err != nil {
+		log.Printf("Error on upgrade (%s)", err)
+	}
+	// read, write, read, write, read, write, read (if close) close
+}
+
 func main(){
-	config := &ssh.ServerConfig{
-		NoClientAuth: true,
-	}
-	privateBytes, err := ioutil.ReadFile(*hostPrivateKey)
-	if err != nil {
-		log.Fatalf("You must set a proper hostkey with -hostkey")
-	}
 
-	private, err := ssh.ParsePrivateKey(privateBytes)
-	if err != nil {
-		log.Fatalf("Failed to parse private key")
-	}
-
-	config.AddHostKey(private)
-
-	listener, err := net.Listen("tcp", "0.0.0.0:22")
-	if err != nil {
-		log.Fatalf("Couldn't open 22: %s", err)
-	}
-
-	log.Print("Opening 22")
-	for {
-		tcpConn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Failed to accept a request (%s)", err)
-			continue
-		}
-		sshConn, chans, reqs, err := ssh.NewServerConn(tcpConn, config)
-		if err != nil {
-			log.Printf("Failed to handshake (%s)", err)
-			continue
-		}
-
-		log.Printf("New SSH cnx from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion())
-
-		// Discard all global out-of-band requests
-		go ssh.DiscardRequests(reqs)
-		// Accept all channels
-		go handleChannels(chans)
-	}
 }
 
-func handleChannels(chans <-chan ssh.NewChannel) {
-	for newChannel := range chans {
-		go handleChannel(newChannel)
-	}
-}
-
-func handleChannel(newChannel ssh.NewChannel) {
-	if t := newChannel.ChannelType(); t != "session" {
-		newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", t))
-		return
-	}
-
-	// At this point, we have the opportunity to reject the client's
-	// request for another logical connection
-	connection, requests, err := newChannel.Accept()
-	if err != nil {
-		log.Printf("Could not accept channel (%s)", err)
-		return
-	}
-
-	// BASH
-	bash := exec.Command("bash")
-
-	// Prep Close
-	close := func() {
-		connection.Close()
-		_, err := bash.Process.Wait()
-		if err != nil {
-			log.Printf("Failed to exit bash (%s)", err)
-		}
-		log.Printf("Session closed")
-	}
-
-	// Allocate a terminal for this channel
-	log.Print("Creating pty...")
-	bashf, err := pty.Start(bash)
-	if err != nil {
-		log.Printf("Could not start pty (%s)", err)
-		close()
-		return
-	}
-
-	// pipe sessions to pty and vis-a-versa
-	var once sync.Once
-	go func() {
-		io.Copy(connection, bashf)
-		once.Do(close)
-	}()
-	go func() {
-		io.Copy(bashf, connection)
-		once.Do(close)
-	}()
-
-	go func() {
-		for req := range requests {
-			switch req.Type {
-			case "shell":
-				// We only accept default shell, no command in payload
-				if len(req.Payload) == 0 {
-					req.Reply(true, nil)
-				}
-			case "pty-req":
-				termLen := req.Payload[3]
-				w, h:= parseDims(req.Payload[termLen+4:])
-				SetWinsize(bashf.Fd(), w, h)
-				req.Reply(true, nil)
-			case "window-change":
-				w, h := parseDims(req.Payload)
-				SetWinsize(bashf.Fd(), w, h)
-			}
-		}
-	}()
-}
-
-// parseDims extracts terminal dimensions (width x height) from the provided buffer
-func parseDims(b []byte) (uint32, uint32) {
-	w := binary.BigEndian.Uint32(b)
-	h := binary.BigEndian.Uint32(b[4:])
-	return w, h
-}
-
-// Winsize stores the Height and Width of a terminal.
-type Winsize struct {
-	Height	uint16
-	Width		uint16
-	x				uint16 // unused
-	y				uint16 // unused
-}
-
-// SetWinsize sets the size of the given pty.
-func SetWinsize(fd uintptr, w, h uint32) {
-	ws := &Winsize{Width: uint16(w), Height: uint16(h)}
-	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSWINSZ), uintptr(unsafe.Pointer(ws)))
-}
