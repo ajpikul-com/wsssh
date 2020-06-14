d770f222230faedfad59b76e07f1f4a06938bc76
Andrew Pikul <ajpikul@gmail.com>
2020-06-14 19:36:45 +0000

    Fix some basic bug errors

--- server/main.go
+++ server/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	//"context"
+	"time"
 	"net/http"
 	"flag"
 	"log"
 
 	"github.com/mangodx/AccessTunnel/sshoverws"
-	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 var hostPrivateKey = flag.String("hostkey", "", "Path to the your private key")
@@ -15,7 +15,7 @@ var hostPrivateKey = flag.String("hostkey", "", "Path to the your private key")
 func init(){
 	flag.Parse()
 	if (len(*hostPrivateKey) == 0) {
-		log.Fatalf("You must set a hostkey with -hostkey")
+		//log.Fatalf("You must set a hostkey with -hostkey")
 	}
 }
 
@@ -26,10 +26,20 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error on upgrade (%s)", err)
 	}
+	_ = conn
 	// read, write, read, write, read, write, read (if close) close
 }
 
 func main(){
-
+	m := mux.NewRouter()
+	m.HandleFunc("/", handleProxy)
+	s := &http.Server {
+		Addr: "127.0.0.1:2223",
+		Handler:	m,
+		ReadTimeout:	10 * time.Second,
+		WriteTimeout:	10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
 
--- sshoverws/main.go
+++ sshoverws/main.go
@@ -10,7 +10,16 @@ import (
 )
 
 var upgrader websocket.Upgrader
-
+func init() {
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:   1024,
+		WriteBufferSize:  1024,
+		HandshakeTimeout: 10*time.Second,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+}
 type WSAddress struct {
 	host string
 }
