7ef7ecd8d960366daf1820f4c7f8f80908d2cad2
Andrew Pikul <ajpikul@gmail.com>
2021-01-15 23:23:57 +0000

    Update main.go of client to use proper logs

--- server/go.mod
+++ server/go.mod
@@ -1,13 +1,14 @@
-module github.com/mangodx/AccessTunnel/server
+module github.com/ayjayt/AccessTunnel/server
 
 go 1.14
 
 require (
+	github.com/ayjayt/ilog v0.0.0-20210115032610-15372227e4a5
 	github.com/creack/pty v1.1.11 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/kr/pty v1.1.8
-	github.com/mangodx/AccessTunnel/sshoverws v0.0.0-20200614220045-064d68c1a884
+	github.com/rogpeppe/gohack v1.0.2 // indirect
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,31 +2,78 @@ package main
 
 import (
 	"time"
+	"net"
 	"net/http"
 	"flag"
-	"log"
 
-	"github.com/mangodx/AccessTunnel/sshoverws"
+	"golang.org/x/crypto/ssh"
+
+	"github.com/ayjayt/ilog"
+	"github.com/ayjayt/AccessTunnel/sshoverws"
 	"github.com/gorilla/mux"
 )
 
 var hostPrivateKey = flag.String("hostkey", "", "Path to the your private key")
 
+var defaultLogger ilog.LoggerInterface
+
+
 func init(){
+	defaultLogger = new(ilog.ZapWrap)
+	err := defaultLogger.Init()
+	if err != nil {
+		panic(err)
+	}
+	sshoverws.SetDefaultLogger(defaultLogger)
 	flag.Parse()
 	if (len(*hostPrivateKey) == 0) {
-		//log.Fatalf("You must set a hostkey with -hostkey")
+		defaultLogger.Error("server main.go flags: No host private key set")
+		defaultLogger.Info("Skipping above error since no auth is implemented yet")
 	}
 }
 
-	// ESTABLISH SSH CONNETION DOWNPIPE
-
 func handleProxy(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Req: %s %s", r.Host, r.URL.Path)
-	conn, err := sshoverws.Upgrade(w, r)
+	defaultLogger.Info("Req: " + r.Host +", " + r.URL.Path) // TODO: So I guess we're taking all paths here?
+	conn, err := sshoverws.Upgrade(w, r) // so it's not a handler
 	if err != nil {
-		log.Printf("Error on upgrade (%s)", err)
+		defaultLogger.Error("AccessTunnel/server/main.go handleProxy sshoverws.Upgrade err: " + err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - sumtin broke")) // TODO: not sure if Upgrade writes a response
+		return
 	}
+	defaultLogger.Info("Setting up new ClientConn to the edge device")
+	sshClientConn, chans, reqs, err := ssh.NewClientConn(conn, "device", &ssh.ClientConfig{
+		User: "",
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			// key is the hosts public key, which has already been certified
+			defaultLogger.Info("Hostname: " + hostname + ", remote: " + remote.Network())
+			return nil
+		},
+	})
+	defer conn.Close()
+	if err != nil {
+		defaultLogger.Error("AccessTunnel/server/main.go handleProxy ssh.NewClientConn err: " + err)
+		return // TODO: A way to close conn w/ message? Multiplex over one message. 
+					 // Client COULD be set to accept text... not binary... as well. // Assumes websockets is up
+	}
+	defaultLogger.Info("Setting up new client")
+	sshClient := ssh.NewClient(sshClientConn, chans, reqs)
+	defaultLogger.Info("Start Session") // Set's up one sessions
+	session, err := sshClient.NewSession()
+	if err != nil {
+		defaultLogger.Error("AccessTunnel/server/main.go sshClient.NewSession() err: " + err)
+		return
+	}
+	defer session.Close()
+	// Do I need to request psuedoterminal?
+	defaultLogger.Info("Calling Shell")
+	err = session.Shell()
+	if err != nil {
+		defaultLogger.Error("AccessTunnel/server/main.go sshClient.Session.Shell() err: " + err)
+		return
+	}
+	defaultLogger.Info("Shell up")
+	session.Wait()
 }
 
 func main(){
@@ -39,6 +86,6 @@ func main(){
 		WriteTimeout:	10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Fatal(s.ListenAndServe())
+	log.Fatal(s.ListenAndServe()) // TODO: Split this up
 }
 
