eca36eb0d44da95632f549af5ca5c2632d869d1c
Andrew Pikul <ajpikul@gmail.com>
2021-01-18 20:19:34 +0000

    Add some better logging to server/main.go

--- server/main.go
+++ server/main.go
@@ -33,7 +33,7 @@ func init(){
 }
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
-	defaultLogger.Info("Req: " + r.Host +", " + r.URL.Path) // TODO: So I guess we're taking all paths here?
+	defaultLogger.Info("INFO: Req: " + r.Host +", " + r.URL.Path) // TODO: So I guess we're taking all paths here?
 	conn, err := sshoverws.Upgrade(w, r) // so it's not a handler
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go handleProxy sshoverws.Upgrade err: " + err.Error())
@@ -41,24 +41,27 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("500 - sumtin broke")) // TODO: not sure if Upgrade writes a response
 		return
 	}
-	defaultLogger.Info("Setting up new ClientConn to the edge device")
+	defaultLogger.Info("INFO: Setting up new ClientConn to the edge device")
 	sshClientConn, chans, reqs, err := ssh.NewClientConn(conn, "device", &ssh.ClientConfig{
 		User: "ajp",
 		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 			// key is the hosts public key, which has already been certified
-			defaultLogger.Info("Hostname: " + hostname + ", remote: " + remote.Network())
+			defaultLogger.Info("INFO: HostkeyCallback: Hostname: " + hostname + ", remote: " + remote.Network())
 			return nil
 		},
 	})
-	defer conn.Close()
+	defer func() {
+		defailtLogger.Info("INFO: Closing upgrade")
+		conn.Close()
+	}()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go handleProxy ssh.NewClientConn err: " + err.Error())
 		return // TODO: A way to close conn w/ message? Multiplex over one message. 
 					 // Client COULD be set to accept text... not binary... as well. // Assumes websockets is up
 	}
-	defaultLogger.Info("Setting up new client")
+	defaultLogger.Info("INFO: Setting up new client")
 	sshClient := ssh.NewClient(sshClientConn, chans, reqs)
-	defaultLogger.Info("Start Session") // Set's up one sessions
+	defaultLogger.Info("INFO: Start Session") // Set's up one sessions
 	session, err := sshClient.NewSession()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go sshClient.NewSession() err: " + err.Error())
@@ -67,7 +70,7 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	defer session.Close()
 	// Do I need to request psuedoterminal? 
 	// What does this do without it?
-	defaultLogger.Info("Calling Shell")
+	defaultLogger.Info("INFO: Calling Shell")
 	err = session.Shell()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go sshClient.Session.Shell() err: " + err.Error())
