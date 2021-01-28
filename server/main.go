969c946839fe755510439ce087f4f9e9ef5581cc
Andrew Pikul <ajpikul@gmail.com>
2021-01-28 17:13:46 +0000

    Add os package for stdin/stdout connection

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 	"flag"
+	"os"
 
 	"golang.org/x/crypto/ssh"
 
@@ -32,7 +33,9 @@ func init(){
 	}
 }
 
+
 func handleProxy(w http.ResponseWriter, r *http.Request) {
+	// Accept websockets connection
 	defaultLogger.Info("INFO: Req: " + r.Host +", " + r.URL.Path) // TODO: So I guess we're taking all paths here?
 	conn, err := sshoverws.Upgrade(w, r) // so it's not a handler
 	if err != nil {
@@ -47,12 +50,13 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 			defaultLogger.Error("AccessTunnel/server/main.go websocket.Conn.Close() err: " + err.Error())
 		}
 	}()
-	defaultLogger.Info("INFO: Sleep")
-	time.Sleep(4 * time.Second)
-	if err := conn.Conn.WriteMessage( 1, []byte("Test Message")); err != nil { // 1 is messagetype TextMessage
-		defaultLogger.Error("Error on test message: " + err.Error())
-		return
+
+	if err = conn.WriteText("Test Text"); err != nil { 
+		defaultLogger.Error("AccessTunnel/server/main.go WriteText(): " + err.Error())
 	}
+
+	// Start the client connection- this is where we identify the client
+	// This is where the globals have been set up.
 	defaultLogger.Info("INFO: Setting an ssh.NewClientConn to the edge device")
 	sshClientConn, chans, reqs, err := ssh.NewClientConn(conn, r.RemoteAddr, &ssh.ClientConfig{
 	//sshClientConn, _, _, err := ssh.NewClientConn(conn, r.RemoteAddr, &ssh.ClientConfig{
@@ -71,36 +75,39 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	}()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go handleProxy ssh.NewClientConn err: " + err.Error())
-		return // TODO: A way to close conn w/ message? Multiplex over one message. 
-			 // Client COULD be set to accept text... not binary... as well. // Assumes websockets is up
+		return
 	}
-
 	defaultLogger.Info("INFO: Setting up new client")
 	sshClient := ssh.NewClient(sshClientConn, chans, reqs)
-	defaultLogger.Info("INFO: Start Session") // Set's up one sessions
+
+	// Now we're asking the client for individual channels. We've already asked for a particular user though. Can we multiplex multple NewClientConns over the same websockets transport?
+	// Also, this sets up streams
+	defaultLogger.Info("INFO: Start Session")
 	session, err := sshClient.NewSession()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go sshClient.NewSession() err: " + err.Error())
 		return
 	}
 	defer session.Close()
-	// Do I need to request psuedoterminal? 
-	// What does this do without it? (my clients ssh server seems to treat every request like a pty-request)
+	// Needs to set up pipes- do these work?
+	session.Stdin = os.Stdin
+	session.Stderr = os.Stderr
+	session.Stdout = os.Stdout
+
+	// All of this basically does reqs
 	defaultLogger.Info("INFO: Calling Shell")
 	err = session.Shell()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go sshClient.Session.Shell() err: " + err.Error())
 		return
 	}
+
+	// Waiting just stays here and keeps the connection open. But basically we want to keep this connection open continuously.
 	defaultLogger.Info("Shell up, setting wait")
-	time.Sleep(10 * time.Second)
 	err = session.Wait()
 	if err != nil {
 		defaultLogger.Error("AccessTunnel/server/main.go session.Wait() err: " + err.Error())
 	}
-	defaultLogger.Info("INFO: Sleep")
-	time.Sleep(10 * time.Second)
-	defaultLogger.Info("Didn't do anything")
 }
 
 func main(){
